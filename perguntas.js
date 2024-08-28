17b77730940bc055627409f3f6eb75f74f683740
zendrini <104445854+zendrini@users.noreply.github.com>
2024-08-28 08:35:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: " ",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    "",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    "",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "",
+                    "",
+                    ""
+                ],
+            },
+        ]
+    }
+];
